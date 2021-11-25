d688629f512ca0571f382ff2f9ff584924d39b92
Quant <quant@quantorium70.ru>
2021-11-25 17:01:34 +0700

    v1.1

--- gololobov.py
+++ gololobov.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from random import randint
 from pathlib import Path
 import os
+from flask import Flask, request
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 book = openpyxl.open("o.xltm", read_only=True)
